ecd82877fe540f64769a649a0de66be91559d7ec
Andi Pätzold <github@andipaetzold.com>
2021-04-29 20:43:26 +0200

    refactor: remove unused ref

--- src/components/View3D/index.tsx
+++ src/components/View3D/index.tsx
@@ -1,6 +1,6 @@
 import { OrthographicCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React from "react";
 import { World } from "../../types";
 import { degreeToRadians } from "../../utils/degreeToRadians";
 import { Bricks } from "./Bricks";
@@ -11,13 +11,10 @@ interface Props {
 }
 
 export function View3D({ world }: Props) {
-  const ref = useRef();
-
   return (
     <Canvas>
       <OrthographicCamera
         makeDefault
-        ref={ref}
         position={[world.width / 2, world.height / 2, world.depth + 2]}
         near={0.1}
         far={world.depth * 2}
