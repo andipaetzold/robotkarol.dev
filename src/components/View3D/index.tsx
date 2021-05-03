566f3ffa88fc1e5862298265b00e703816822f52
Andi Pätzold <github@andipaetzold.com>
2021-05-03 21:58:25 +0200

    chore: remove dead code

--- src/components/View3D/index.tsx
+++ src/components/View3D/index.tsx
@@ -1,20 +1,10 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { Matrix4, WebGLRenderer } from "three";
+import { WebGLRenderer } from "three";
 import useResizeObserver from "use-resize-observer";
 import { World } from "../../types";
 import { createCamera } from "./camera";
 import styles from "./index.module.scss";
 import { createScene } from "./scene";
-
-const alpha = Math.PI / 6; // or Math.PI / 4
-const Syx = 0,
-  Szx = -0.5 * Math.cos(alpha),
-  Sxy = 0,
-  Szy = -0.5 * Math.sin(alpha),
-  Sxz = 0,
-  Syz = 0;
-const matrix = new Matrix4();
-matrix.set(1, Syx, Szx, 0, Sxy, 1, Szy, 0, Sxz, Syz, 1, 0, 0, 0, 0, 1);
 
 interface Props {
   world: World;
@@ -29,7 +19,12 @@ export function View3D({ world }: Props) {
       return;
     }
 
-    const r = new WebGLRenderer({ canvas, alpha: true, antialias: true, precision: 'highp' });
+    const r = new WebGLRenderer({
+      canvas,
+      alpha: true,
+      antialias: true,
+      precision: "highp",
+    });
     return r;
   }, [canvas]);
 
