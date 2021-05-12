987755b1d0570746644af41d14ae4451b4238cbc
Andi Pätzold <github@andipaetzold.com>
2021-05-13 01:28:08 +0200

    test: split tests and snapshot results

--- src/test/integration/examples.spec.ts
+++ src/test/integration/examples.spec.ts
@@ -2,48 +2,70 @@ import { readFileSync } from "fs";
 import path from "path";
 import { EXAMPLES } from "../../services/examples";
 import { parse } from "../../services/parser";
-import { AST } from "../../services/parser/types";
 import { readWorld } from "../../services/reader";
 import { executionStep } from "../../services/store/reducers/executionStep";
 import { RootState } from "../../services/store/types";
-import { World } from "../../types";
 
 EXAMPLES.forEach((example) => {
-  it(`${example.name}`, () => {
-    let world: World | undefined;
-    if (example.world) {
-      const worldRaw = readFileSync(
-        path.resolve(`${__dirname}/../../../public/examples/${example.world}`),
-        { encoding: "utf8" }
-      );
-      world = readWorld(worldRaw);
+  describe(`${example.name}`, () => {
+    if (!example.code && example.world) {
+      it("can read world", () => {
+        const worldRaw = readFileSync(
+          path.resolve(
+            `${__dirname}/../../../public/examples/${example.world}`
+          ),
+          { encoding: "utf8" }
+        );
+        const world = readWorld(worldRaw);
+
+        expect(world).toMatchSnapshot();
+      });
     }
 
-    let code = "";
-    let ast: AST | undefined;
-    if (example.code) {
-      code = readFileSync(
-        path.resolve(`${__dirname}/../../../public/examples/${example.code}`),
-        { encoding: "utf8" }
-      );
-      parse(code);
+    if (example.code && !example.world) {
+      it("can read code", () => {
+        const code = readFileSync(
+          path.resolve(`${__dirname}/../../../public/examples/${example.code}`),
+          { encoding: "utf8" }
+        );
+        const ast = parse(code);
+        expect(ast).toMatchSnapshot();
+      });
     }
 
-    if (world && ast) {
-      const state: RootState = {
-        code,
-        world,
-        execution: {
-          speed: "slow",
-          stack: ast.program.body,
-          state: "running",
-          ast,
-        },
-      };
+    if (example.code && example.world) {
+      it("can run program", () => {
+        const worldRaw = readFileSync(
+          path.resolve(
+            `${__dirname}/../../../public/examples/${example.world}`
+          ),
+          { encoding: "utf8" }
+        );
+        const world = readWorld(worldRaw);
 
-      while (state.execution.state !== "done") {
-        executionStep(state);
-      }
+        const code = readFileSync(
+          path.resolve(`${__dirname}/../../../public/examples/${example.code}`),
+          { encoding: "utf8" }
+        );
+        const ast = parse(code);
+
+        const state: RootState = {
+          code,
+          world,
+          execution: {
+            speed: "slow",
+            stack: ast.program.body,
+            state: "running",
+            ast,
+          },
+        };
+
+        while (state.execution.state !== "done") {
+          executionStep(state);
+        }
+
+        expect(state.world).toMatchSnapshot();
+      });
     }
   });
 });
