534dfca1f37aaaced623921fcbfdf2d6dbd9083a
Andi Pätzold <github@andipaetzold.com>
2021-05-12 23:45:08 +0200

    fix: finish execution on error

--- src/services/store/reducers/executionStep.ts
+++ src/services/store/reducers/executionStep.ts
@@ -13,82 +13,90 @@ export function executionStep(state: RootState): void {
     state.execution.worldOnStart = state.world;
   }
 
-  let didCall = false;
-  while (state.execution.stack.length > 0) {
-    const statement = state.execution.stack.shift()!;
-    state.execution.activeLine = statement.line;
+  try {
+    let didCall = false;
+    while (state.execution.stack.length > 0) {
+      const statement = state.execution.stack.shift()!;
+      state.execution.activeLine = statement.line;
 
-    switch (statement.type) {
-      case "call": {
-        state.world = doCall(statement, state.world);
-        didCall = true;
-        break;
-      }
-      case "systemCall": {
-        switch (statement.action) {
-          case "fast":
-            state.execution.speed = "fast";
-            break;
-          case "slow":
-            state.execution.speed = "slow";
-            break;
-          case "exit":
-            state.execution.stack = [];
-            break;
+      switch (statement.type) {
+        case "call": {
+          state.world = doCall(statement, state.world);
+          didCall = true;
+          break;
         }
-        break;
-      }
-      case "functionCall": {
-        const functionCall = state.execution.ast?.functions.find(
-          (x) => x.identifier === statement.name
-        )!;
-        state.execution.stack.unshift({
-          type: "systemCall",
-          line: statement.line,
-          action: "slow",
-        });
-        state.execution.stack.unshift(...functionCall.body);
-        break;
-      }
-      case "repeat": {
-        if (statement.times > 1) {
+        case "systemCall": {
+          switch (statement.action) {
+            case "fast":
+              state.execution.speed = "fast";
+              break;
+            case "slow":
+              state.execution.speed = "slow";
+              break;
+            case "exit":
+              state.execution.stack = [];
+              break;
+          }
+          break;
+        }
+        case "functionCall": {
+          const functionCall = state.execution.ast?.functions.find(
+            (x) => x.identifier === statement.name
+          )!;
           state.execution.stack.unshift({
-            ...statement,
-            times: statement.times - 1,
+            type: "systemCall",
+            line: statement.line,
+            action: "slow",
           });
+          state.execution.stack.unshift(...functionCall.body);
+          break;
         }
-        state.execution.stack.unshift(...statement.body);
-        break;
-      }
-      case "if": {
-        if (checkCondition(statement.condition, state.world)) {
+        case "repeat": {
+          if (statement.times > 1) {
+            state.execution.stack.unshift({
+              ...statement,
+              times: statement.times - 1,
+            });
+          }
           state.execution.stack.unshift(...statement.body);
-        } else {
-          state.execution.stack.unshift(...statement.elseBody);
+          break;
         }
-        break;
-      }
-      case "while": {
-        if (checkCondition(statement.condition, state.world)) {
-          state.execution.stack.unshift(...statement.body, statement);
+        case "if": {
+          if (checkCondition(statement.condition, state.world)) {
+            state.execution.stack.unshift(...statement.body);
+          } else {
+            state.execution.stack.unshift(...statement.elseBody);
+          }
+          break;
+        }
+        case "while": {
+          if (checkCondition(statement.condition, state.world)) {
+            state.execution.stack.unshift(...statement.body, statement);
+          }
         }
       }
-    }
 
-    let finish = false;
-    if (didCall && state.execution.speed === "slow") {
-      finish = true;
-    }
+      let finish = false;
+      if (didCall && state.execution.speed === "slow") {
+        finish = true;
+      }
 
-    if (
-      state.execution.stack.filter((s) => s.type !== "systemCall").length === 0
-    ) {
-      state.execution.state = "done";
-      finish = true;
-    }
+      if (
+        state.execution.stack.filter((s) => s.type !== "systemCall").length ===
+        0
+      ) {
+        state.execution.state = "done";
+        finish = true;
+      }
 
-    if (finish) {
-      return;
+      if (finish) {
+        return;
+      }
     }
+  } catch (e) {
+    state.execution.state = "done";
+
+    // TODO: handle error
+    throw e;
   }
 }
