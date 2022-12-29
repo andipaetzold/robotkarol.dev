ca34f26e1e2bda83e94fdcb67eee9503c4927e7e
Andi Pätzold <github@andipaetzold.com>
2022-12-29 21:02:11 +0100

    Revert "test: ensure code and world files exist"
    This reverts commit 39ab84324d53518ee7cb21fdbbb22e86b937e8ae.

--- src/services/examples.spec.ts
+++ src/services/examples.spec.ts
@@ -1,25 +1,9 @@
 import { describe, expect, it } from "vitest";
 import { EXAMPLES } from "./examples";
-import { existsSync } from "fs";
-import { resolve } from "path";
 
 describe("EXAMPLES", () => {
   it("name is unique", () => {
     const names = EXAMPLES.map((e) => e.name);
     expect(new Set(names).size).toBe(names.length);
-  });
-
-  describe.each(EXAMPLES)("$name", (example) => {
-    if (example.code) {
-      it("code file exists", () => {
-        expect(existsSync(`public/examples/${example.code!}`)).toBe(true);
-      });
-    }
-
-    if (example.world) {
-      it("world file exists", () => {
-        expect(existsSync(`public/examples/${example.world}`)).toBe(true);
-      });
-    }
   });
 });
